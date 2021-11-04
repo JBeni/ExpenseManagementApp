db2543b3d2f98abfe249246eb602520e533d4a9c
JBeni <JBeni@users.noreply.github.com>
2021-11-04 22:09:25 +0200

    creating aad trip form and check for constraints

--- app/src/main/java/com/example/app/AddActivity.java
+++ app/src/main/java/com/example/app/AddActivity.java
@@ -1,32 +1,111 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText title_input, author_input, pages_input;
-    Button add_button;
+    EditText name, destination, date, risk_assessment, description, duration, aim, status;
+    Button save_button;
+
+    private boolean allConditionChecked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        title_input = findViewById(R.id.title_input);
-        author_input = findViewById(R.id.author_input);
-        // pages_input = findViewById(R.id.pages_input);
-        add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(new View.OnClickListener() {
+        name = findViewById(R.id.name_column);
+        destination = findViewById(R.id.destination_column);
+        date = findViewById(R.id.date_column);
+        risk_assessment = findViewById(R.id.risk_assessment_column);
+        description = findViewById(R.id.description_column);
+        duration = findViewById(R.id.duration_column);
+        aim = findViewById(R.id.aim_column);
+        status = findViewById(R.id.status_column);
+
+        checkEditTextErrors(name);
+        checkEditTextErrors(destination);
+        checkEditTextErrors(date);
+        checkEditTextErrors(risk_assessment);
+        checkEditTextErrors(description);
+        checkEditTextErrors(duration);
+        checkEditTextErrors(aim);
+        checkEditTextErrors(status);
+
+/*
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (name.getText().toString().length() <= 3) {
+                    name.setError("The value must have at least 4 characters.");
+                } else {
+                    name.setError(null);
+                }
+            }
+        });
+*/
+        save_button = findViewById(R.id.save_trip_db_button);
+        save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseSqlite myDB = new DatabaseSqlite(AddActivity.this);
-                myDB.addBook(title_input.getText().toString().trim(),
-                        author_input.getText().toString().trim(),
-                        Integer.valueOf(3));
+                isEditTextEmpty(name);
+                isEditTextEmpty(destination);
+                isEditTextEmpty(date);
+                isEditTextEmpty(risk_assessment);
+                isEditTextEmpty(description);
+                isEditTextEmpty(duration);
+                isEditTextEmpty(aim);
+                isEditTextEmpty(status);
+
+                if (allConditionChecked) {
+                    DatabaseSqlite myDB = new DatabaseSqlite(AddActivity.this);
+                    myDB.addBook(name.getText().toString().trim(), destination.getText().toString().trim(), Integer.valueOf(3));
+                } else {
+                    allConditionChecked = true;
+                }
+            }
+        });
+    }
+
+    public void isEditTextEmpty(EditText textName) {
+        if (textName.getText().toString().length() == 0) {
+            textName.setError("This field is required.");
+            allConditionChecked = false;
+        } else if (textName.getText().toString().length() <= 2) {
+            textName.setError("The value must have at least 3 characters.");
+            allConditionChecked = false;
+        }
+    }
+
+    public void checkEditTextErrors(EditText textName) {
+        textName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (textName.getText().toString().length() <= 2) {
+                    textName.setError("The value must have at least 3 characters.");
+                }
             }
         });
     }
