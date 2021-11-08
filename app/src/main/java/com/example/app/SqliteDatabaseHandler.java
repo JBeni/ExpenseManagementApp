802b48972994ee8ad4eaa31ddc34c2ada3e467ed
JBeni <JBeni@users.noreply.github.com>
2021-11-08 18:58:28 +0200

    adding methods for crud for trip and expenses - database

--- app/src/main/java/com/example/app/SqliteDatabaseHandler.java
+++ app/src/main/java/com/example/app/SqliteDatabaseHandler.java
@@ -14,39 +14,34 @@ import java.util.List;
 public class SqliteDatabaseHandler extends SQLiteOpenHelper {
 
     List<Trip> tripsList = new ArrayList<>();
+    List<Expense> expensesList = new ArrayList<>();
+    List<Expense> tripExpensesList = new ArrayList<>();
     private SQLiteDatabase database;
     Context context;
 
     private static final String DATABASE_NAME = "ExpenseManagement.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String TRIP_TABLE_NAME = "Trips";
-    public static final String ID_COLUMN = "id";
-    public static final String NAME_COLUMN = "name";
-    public static final String DESTINATION_COLUMN = "destination";
-    public static final String DATE_COLUMN = "date";
-    public static final String RISK_ASSESSMENT_COLUMN = "risk_assessment";
-    public static final String DESCRIPTION_COLUMN = "description";
-    public static final String DURATION_COLUMN = "duration";
-    public static final String AIM_COLUMN = "aim";
-    public static final String STATUS_COLUMN = "status";
+    //region TABLES DATA
+        private static final String TRIP_TABLE_NAME = "Trips";
+        public static final String TRIP_ID_COLUMN = "id";
+        public static final String TRIP_NAME_COLUMN = "name";
+        public static final String TRIP_DESTINATION_COLUMN = "destination";
+        public static final String TRIP_DATE_COLUMN = "date";
+        public static final String TRIP_RISK_ASSESSMENT_COLUMN = "risk_assessment";
+        public static final String TRIP_DESCRIPTION_COLUMN = "description";
+        public static final String TRIP_DURATION_COLUMN = "duration";
+        public static final String TRIP_AIM_COLUMN = "aim";
+        public static final String TRIP_STATUS_COLUMN = "status";
 
-    private static final String EXPENSE_TABLE_NAME = "Expenses";
-    public static final String EXPENSE_ID_COLUMN = "id";
-    public static final String EXPENSE_TYPE_COLUMN = "type";
-    public static final String EXPENSE_AMOUNT_COLUMN = "amount";
-    public static final String EXPENSE_TIME_COLUMN = "time";
-    public static final String EXPENSE_COMMENTS_COLUMN = "additional_comments";
-    public static final String EXPENSE_TRIP_ID_COLUMN = "trip_id";
-
-
-    private static final String TABLE_NAME = "contacts";
-    private static final String KEY_ROWID = "_id";
-    private static final String KEY_NAME="name";
-    private static final String KEY_EMAIL = "email";
-
-    private static final String CREATE_CONTACTS_TABLE = "create table "+ TABLE_NAME+
-            " ("+ KEY_ROWID+" integer primary key autoincrement, "+ KEY_NAME + " text, "+ KEY_EMAIL+ " text)";
+        private static final String EXPENSE_TABLE_NAME = "Expenses";
+        public static final String EXPENSE_ID_COLUMN = "id";
+        public static final String EXPENSE_TYPE_COLUMN = "type";
+        public static final String EXPENSE_AMOUNT_COLUMN = "amount";
+        public static final String EXPENSE_TIME_COLUMN = "time";
+        public static final String EXPENSE_COMMENTS_COLUMN = "additional_comments";
+        public static final String EXPENSE_TRIP_ID_COLUMN = "trip_id";
+    //endregion
 
     public SqliteDatabaseHandler(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -67,8 +62,8 @@ public class SqliteDatabaseHandler extends SQLiteOpenHelper {
                         "   %s TEXT, " +
                         "   %s TEXT, " +
                         "   %s TEXT)",
-                TRIP_TABLE_NAME, ID_COLUMN, NAME_COLUMN, DESTINATION_COLUMN, DATE_COLUMN,
-                RISK_ASSESSMENT_COLUMN, DESCRIPTION_COLUMN, DURATION_COLUMN, AIM_COLUMN, STATUS_COLUMN);
+                TRIP_TABLE_NAME, TRIP_ID_COLUMN, TRIP_NAME_COLUMN, TRIP_DESTINATION_COLUMN, TRIP_DATE_COLUMN,
+                TRIP_RISK_ASSESSMENT_COLUMN, TRIP_DESCRIPTION_COLUMN, TRIP_DURATION_COLUMN, TRIP_AIM_COLUMN, TRIP_STATUS_COLUMN);
 
         String queryExpense = String.format(
                 "CREATE TABLE %s (" +
@@ -84,119 +79,144 @@ public class SqliteDatabaseHandler extends SQLiteOpenHelper {
 
         db.execSQL(queryTrip);
         db.execSQL(queryExpense);
-        db.execSQL(CREATE_CONTACTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TRIP_TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + EXPENSE_TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         Log.v(this.getClass().getName(), DATABASE_NAME + " database upgrade to version " + newVersion + " - old data lost");
         onCreate(db);
     }
 
-    public void insertTrip(String trip_name, String trip_destination, String trip_date, String risk, String description, String duration, String aim, String status) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void insertTrip(String name, String destination, String date, String risk, String description, String duration, String aim, String status) {
         ContentValues rowValues = new ContentValues();
 
-        rowValues.put(NAME_COLUMN, trip_name);
-        rowValues.put(DESTINATION_COLUMN, trip_destination);
-        rowValues.put(DATE_COLUMN, trip_date);
-        rowValues.put(RISK_ASSESSMENT_COLUMN, risk);
-        rowValues.put(DESCRIPTION_COLUMN, description);
-        rowValues.put(DURATION_COLUMN, duration);
-        rowValues.put(AIM_COLUMN, aim);
-        rowValues.put(STATUS_COLUMN, status);
+        rowValues.put(TRIP_NAME_COLUMN, name);
+        rowValues.put(TRIP_DESTINATION_COLUMN, destination);
+        rowValues.put(TRIP_DATE_COLUMN, date);
+        rowValues.put(TRIP_RISK_ASSESSMENT_COLUMN, risk);
+        rowValues.put(TRIP_DESCRIPTION_COLUMN, description);
+        rowValues.put(TRIP_DURATION_COLUMN, duration);
+        rowValues.put(TRIP_AIM_COLUMN, aim);
+        rowValues.put(TRIP_STATUS_COLUMN, status);
 
-        long result = db.insert(TRIP_TABLE_NAME, null, rowValues);
+        long result = database.insert(TRIP_TABLE_NAME, null, rowValues);
         if (result == -1) {
-            Toast.makeText(context.getApplicationContext(), "Insert operation has failed!", Toast.LENGTH_LONG).show();
+            Toast.makeText(context.getApplicationContext(), "Insert Trip operation has failed!", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(context.getApplicationContext(), "Insert operation was successful!", Toast.LENGTH_LONG).show();
+            Toast.makeText(context.getApplicationContext(), "Insert Trip operation was successful!", Toast.LENGTH_LONG).show();
         }
     }
 
-    /**
-     * Code inspired from https://github.com/stevdza-san/SQLite_Android-Complete_Tutorial/blob/master/app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MyDatabaseHelper.java
-     */
-    public void updateTrip(String trip_id, String trip_name, String trip_destination, String trip_date, String risk, String description, String duration, String aim, String status) {
+    public void insertExpense(String type, String amount, String time, String additional_comments, String trip_id) {
         ContentValues rowValues = new ContentValues();
 
-        rowValues.put(NAME_COLUMN, trip_name);
-        rowValues.put(DESTINATION_COLUMN, trip_destination);
-        rowValues.put(DATE_COLUMN, trip_date);
-        rowValues.put(RISK_ASSESSMENT_COLUMN, risk);
-        rowValues.put(DESCRIPTION_COLUMN, description);
-        rowValues.put(DURATION_COLUMN, duration);
-        rowValues.put(AIM_COLUMN, aim);
-        rowValues.put(STATUS_COLUMN, status);
+        rowValues.put(EXPENSE_TYPE_COLUMN, type);
+        rowValues.put(EXPENSE_AMOUNT_COLUMN, amount);
+        rowValues.put(EXPENSE_TIME_COLUMN, time);
+        rowValues.put(EXPENSE_COMMENTS_COLUMN, additional_comments);
+        rowValues.put("trip_id", trip_id);
 
-        long result = database.update(TRIP_TABLE_NAME, rowValues, "id=?", new String[]{ trip_id });
+        long result = database.insert(EXPENSE_TABLE_NAME, null, rowValues);
         if (result == -1) {
-            Toast.makeText(context, "Update operation has failed!", Toast.LENGTH_LONG).show();
+            Toast.makeText(context.getApplicationContext(), "Insert Expense operation has failed!", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(context, "Update operation was successful!", Toast.LENGTH_LONG).show();
+            Toast.makeText(context.getApplicationContext(), "Insert Expense operation was successful!", Toast.LENGTH_LONG).show();
         }
     }
 
-    /**
-     * Code inspired from https://github.com/stevdza-san/SQLite_Android-Complete_Tutorial/blob/master/app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MyDatabaseHelper.java
-     */
-    public void deleteTrip(String trip_id) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        long result = database.delete(TRIP_TABLE_NAME, "id=?", new String[]{ trip_id });
+    public void updateTrip(String id, String name, String destination, String date, String risk, String description, String duration, String aim, String status) {
+        ContentValues rowValues = new ContentValues();
+
+        rowValues.put(TRIP_NAME_COLUMN, name);
+        rowValues.put(TRIP_DESTINATION_COLUMN, destination);
+        rowValues.put(TRIP_DATE_COLUMN, date);
+        rowValues.put(TRIP_RISK_ASSESSMENT_COLUMN, risk);
+        rowValues.put(TRIP_DESCRIPTION_COLUMN, description);
+        rowValues.put(TRIP_DURATION_COLUMN, duration);
+        rowValues.put(TRIP_AIM_COLUMN, aim);
+        rowValues.put(TRIP_STATUS_COLUMN, status);
+
+        // Code from https://stackoverflow.com/questions/9798473/sqlite-in-android-how-to-update-a-specific-row
+        long result = database.update(TRIP_TABLE_NAME, rowValues, "id=?", new String[]{ id });
         if (result == -1) {
-            Toast.makeText(context, "Delete operation has failed!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Update Trip operation has failed!", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(context, "Delete operation was successful!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Update Trip operation was successful!", Toast.LENGTH_LONG).show();
         }
     }
 
-    /**
-     * Code inspired from https://github.com/stevdza-san/SQLite_Android-Complete_Tutorial/blob/master/app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MyDatabaseHelper.java
-     */
-    public void deleteAllTrips() {
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void updateExpense(String id, String type, String amount, String time, String additional_comments) {
+        ContentValues rowValues = new ContentValues();
+
+        rowValues.put(EXPENSE_TYPE_COLUMN, type);
+        rowValues.put(EXPENSE_AMOUNT_COLUMN, amount);
+        rowValues.put(EXPENSE_TIME_COLUMN, time);
+        rowValues.put(EXPENSE_COMMENTS_COLUMN, additional_comments);
+
+        long result = database.update(EXPENSE_TABLE_NAME, rowValues, "id=?", new String[]{ id });
+        if (result == -1) {
+            Toast.makeText(context, "Update Expense operation has failed!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Update Expense operation was successful!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void deleteTrip(String id) {
+        long result = database.delete(TRIP_TABLE_NAME, "id=?", new String[]{ id });
+        if (result == -1) {
+            Toast.makeText(context, "Delete Trip operation has failed!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Delete Trip operation was successful!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void deleteExpense(String id) {
+        long result = database.delete(EXPENSE_TABLE_NAME, "id=?", new String[]{ id });
+        if (result == -1) {
+            Toast.makeText(context, "Delete Expense operation has failed!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Delete Expense operation was successful!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void deleteAllData() {
+        database.execSQL("DELETE FROM " + EXPENSE_TABLE_NAME);
         database.execSQL("DELETE FROM " + TRIP_TABLE_NAME);
     }
 
-    /**
-     * Code inspired from https://github.com/stevdza-san/SQLite_Android-Complete_Tutorial/blob/master/app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MyDatabaseHelper.java
-     */
-    public Cursor getTrips() {
-        String query =" SELECT * FROM " + TRIP_TABLE_NAME;
-        SQLiteDatabase db = this.getReadableDatabase();
-
+    public Trip getTrip(String trip_id) {
         Cursor cursor = null;
         if (database != null) {
-           cursor = database.rawQuery(query, null);
+            cursor = database.rawQuery("SELECT * FROM " + TRIP_TABLE_NAME + " WHERE " + TRIP_ID_COLUMN + "=?", new String[] { trip_id });
         }
-        return cursor;
+        cursor.moveToFirst();
+
+        int id = cursor.getInt(0);
+        String name = cursor.getString(1);
+        String destination = cursor.getString(2);
+        String date = cursor.getString(3);
+        String risk_assessment = cursor.getString(4);
+        String description = cursor.getString(5);
+        String duration = cursor.getString(6);
+        String aim = cursor.getString(7);
+        String status = cursor.getString(8);
+
+        return new Trip(id, name, destination, date, risk_assessment, description, duration, aim, status);
     }
 
     /**
-     * https://github.com/sandipapps/Populate-RecyclerView-From-SQLite
+     * Code inspired from https://stackoverflow.com/questions/14266575/getting-all-records-from-sqlite-android/14266660
      */
     public List<Trip> getAllTrips() {
         String query =" SELECT * FROM " + TRIP_TABLE_NAME;
-        SQLiteDatabase db = this.getReadableDatabase();
-
         Cursor cursor = null;
         if (database != null) {
             cursor = database.rawQuery(query, null);
         }
 
-        while(cursor.moveToNext()){
-            /*
-            int index1 = cursor.getColumnIndex(KEY_ROWID);
-            int rowid = cursor.getInt(index1);
-            int index2 = cursor.getColumnIndex(KEY_NAME);
-            String name = cursor.getString(index2);
-            int index3 = cursor.getColumnIndex(KEY_EMAIL);
-            String email = cursor.getString(index3);
-            */
-
+        while(cursor.moveToNext()) {
             int id = cursor.getInt(0);
             String name = cursor.getString(1);
             String destination = cursor.getString(2);
@@ -207,9 +227,67 @@ public class SqliteDatabaseHandler extends SQLiteOpenHelper {
             String aim = cursor.getString(7);
             String status = cursor.getString(8);
 
-            Trip contact = new Trip(id, name, destination, date, risk_assessment, description, duration, aim, status);
-            tripsList.add(contact);
+            Trip trip = new Trip(id, name, destination, date, risk_assessment, description, duration, aim, status);
+            tripsList.add(trip);
         }
         return tripsList;
+    }
+
+    /**
+     * https://stackoverflow.com/questions/53219223/get-specific-row-of-sqlite-android
+     */
+    public Expense getExpense(String expense_id) {
+        Cursor cursor = null;
+        if (database != null) {
+            cursor = database.rawQuery("SELECT * FROM " + EXPENSE_TABLE_NAME + " WHERE " + EXPENSE_ID_COLUMN + "=?", new String[] { expense_id });
+        }
+        cursor.moveToFirst();
+
+        int id = cursor.getInt(0);
+        String type = cursor.getString(1);
+        String amount = cursor.getString(2);
+        String time = cursor.getString(3);
+        String additional_comments = cursor.getString(4);
+
+        return new Expense(id, type, amount, time, additional_comments);
+    }
+
+    public List<Expense> getAllExpenses() {
+        String query =" SELECT * FROM " + EXPENSE_TABLE_NAME;
+        Cursor cursor = null;
+        if (database != null) {
+            cursor = database.rawQuery(query, null);
+        }
+
+        while(cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String type = cursor.getString(1);
+            String amount = cursor.getString(2);
+            String time = cursor.getString(3);
+            String additional_comments = cursor.getString(4);
+
+            Expense expense = new Expense(id, type, amount, time, additional_comments);
+            expensesList.add(expense);
+        }
+        return expensesList;
+    }
+
+    public List<Expense> getTripExpenses(String trip_id) {
+        Cursor cursor = null;
+        if (database != null) {
+            cursor = database.rawQuery("SELECT * FROM " + EXPENSE_TABLE_NAME + " WHERE trip_id=?", new String[] { trip_id });
+        }
+
+        while(cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String type = cursor.getString(1);
+            String amount = cursor.getString(2);
+            String time = cursor.getString(3);
+            String additional_comments = cursor.getString(4);
+
+            Expense expense = new Expense(id, type, amount, time, additional_comments);
+            tripExpensesList.add(expense);
+        }
+        return tripExpensesList;
     }
 }
