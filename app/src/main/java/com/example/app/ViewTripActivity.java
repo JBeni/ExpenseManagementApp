588ef8f9eaff5b3dd1c6464843b0f04f8c5a42c5
JBeni <JBeni@users.noreply.github.com>
2021-11-09 22:08:25 +0200

    implement the view trip activity

--- app/src/main/java/com/example/app/ViewTripActivity.java
+++ app/src/main/java/com/example/app/ViewTripActivity.java
@@ -1,42 +1,40 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.widget.TextView;
 
 public class ViewTripActivity extends AppCompatActivity {
 
-    FloatingActionButton update_button, delete_button;
+    TextView name, destination, date, risk_assessment, description, duration, aim, status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_trip);
 
-/*
-        update_button = findViewById(R.id.edit_expense_button);
-        delete_button = findViewById(R.id.delete_expense_button);
+        name = findViewById(R.id.view_name_trip_column);
+        destination = findViewById(R.id.view_destination_trip_column);
+        date = findViewById(R.id.view_date_trip_column);
+        risk_assessment = findViewById(R.id.view_risk_assessment_trip_column);
+        description = findViewById(R.id.view_description_trip_column);
+        duration = findViewById(R.id.view_duration_trip_column);
+        aim = findViewById(R.id.view_aim_trip_column);
+        status = findViewById(R.id.view_status_trip_column);
 
-        update_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //And only then we call this
-                //SqliteDatabaseHandler myDB = new SqliteDatabaseHandler(UpdateTripActivity.this);
-                //title = title_input.getText().toString().trim();
-                //author = author_input.getText().toString().trim();
-                //pages = "3";
-                //myDB.updateData(id, title, author, pages);
-            }
-        });
-        delete_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //confirmDialog();
-            }
-        });
-*/
+        if (getIntent().hasExtra("id") && getIntent().hasExtra("name") &&
+            getIntent().hasExtra("destination") && getIntent().hasExtra("date") &&
+            getIntent().hasExtra("risk_assessment") && getIntent().hasExtra("duration") &&
+            getIntent().hasExtra("status")
+        ) {
+            name.setText(getIntent().getStringExtra("name"));
+            destination.setText(getIntent().getStringExtra("destination"));
+            date.setText(getIntent().getStringExtra("date"));
+            risk_assessment.setText(getIntent().getStringExtra("risk_assessment"));
+            description.setText(getIntent().getStringExtra("description"));
+            duration.setText(getIntent().getStringExtra("duration"));
+            aim.setText(getIntent().getStringExtra("aim"));
+            status.setText(getIntent().getStringExtra("status"));
+        }
     }
-
 }
