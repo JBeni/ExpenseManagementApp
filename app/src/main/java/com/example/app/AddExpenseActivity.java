3397c500ca57ebd731af67622489d4780389926a
JBeni <JBeni@users.noreply.github.com>
2021-11-10 13:07:44 +0200

    update add expense activity

--- app/src/main/java/com/example/app/AddExpenseActivity.java
+++ app/src/main/java/com/example/app/AddExpenseActivity.java
@@ -1,15 +1,19 @@
 package com.example.app;
 
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddExpenseActivity extends AppCompatActivity {
-
+    TimePickerDialog picker;
     EditText type, amount, time, additional_comments;
     Button save_button;
     private boolean allConditionChecked = true;
@@ -19,44 +23,28 @@ public class AddExpenseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
 
-        type = findViewById(R.id.type_expense_column);
-        amount = findViewById(R.id.amount_expense_column);
-        time = findViewById(R.id.time_expense_column);
-        additional_comments = findViewById(R.id.additional_comments_expense_column);
+        type = findViewById(R.id.add_type_expense_column);
+        amount = findViewById(R.id.add_amount_expense_column);
+        additional_comments = findViewById(R.id.add_additional_comments_expense_column);
 
-        checkEditTextErrors(type);
-        checkEditTextErrors(amount);
-        checkEditTextErrors(time);
-        checkEditTextErrors(additional_comments);
+        time = findViewById(R.id.add_time_expense_column);
+        time.setFocusable(false);
+        time.setInputType(InputType.TYPE_NULL);
+        addTimePicker(time);
 
-/*
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
+        checkTextErrors(type);
+        checkTextErrors(amount);
+        checkTextErrors(time);
+        checkTextErrors(additional_comments);
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (name.getText().toString().length() <= 3) {
-                    name.setError("The value must have at least 4 characters.");
-                } else {
-                    name.setError(null);
-                }
-            }
-        });
-*/
-
-        save_button = findViewById(R.id.save_expense_db_button_text);
+        save_button = findViewById(R.id.add_save_expense_db_button);
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isEditTextEmpty(type);
-                isEditTextEmpty(amount);
-                isEditTextEmpty(time);
-                isEditTextEmpty(additional_comments);
+                isTextEmpty(type);
+                isTextEmpty(amount);
+                isTextEmpty(time);
+                isTextEmpty(additional_comments);
 
                 if (allConditionChecked) {
                     SqliteDatabaseHandler databaseHandler = new SqliteDatabaseHandler(AddExpenseActivity.this);
@@ -72,7 +60,30 @@ public class AddExpenseActivity extends AppCompatActivity {
         });
     }
 
-    public void isEditTextEmpty(EditText textName) {
+    /**
+     * https://www.tutlane.com/tutorial/android/android-timepicker-with-examples
+     */
+    public void addTimePicker(EditText eText) {
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr.get(Calendar.MINUTE);
+
+                picker = new TimePickerDialog(AddExpenseActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                        eText.setText(sHour + ":" + sMinute);
+                    }
+                }, hour, minutes, true);
+                picker.setCancelable(false);
+                picker.show();
+            }
+        });
+    }
+
+    public void isTextEmpty(EditText textName) {
         if (textName.getText().toString().length() == 0) {
             textName.setError("This field is required.");
             allConditionChecked = false;
@@ -82,7 +93,7 @@ public class AddExpenseActivity extends AppCompatActivity {
         }
     }
 
-    public void checkEditTextErrors(EditText textName) {
+    public void checkTextErrors(EditText textName) {
         textName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
