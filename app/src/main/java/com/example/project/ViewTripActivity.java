047737df644f0a46853c7356b55e23b177b97950
JBeni <JBeni@users.noreply.github.com>
2021-11-23 13:35:41 +0200

    update view trip activity class

--- app/src/main/java/com/example/project/ViewTripActivity.java
+++ app/src/main/java/com/example/project/ViewTripActivity.java
@@ -1,18 +1,17 @@
 package com.example.project;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class ViewTripActivity extends AppCompatActivity {
-
     TextView name, destination, date, risk_assessment, description, duration, aim, status;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,22 +42,19 @@ public class ViewTripActivity extends AppCompatActivity {
         }
 
         BottomNavigationView bottomNavigation = findViewById(R.id.navigation_bottom);
-        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home_navigation:
-                        startActivity(new Intent(getApplicationContext(), MainTripActivity.class));
-                        break;
-                    case R.id.search_navigation:
-                        startActivity(new Intent(getApplicationContext(), SearchActivity.class));
-                        break;
-                    case R.id.settings_navigation:
-                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
-                        break;
-                }
-                return true;
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home_navigation:
+                    startActivity(new Intent(getApplicationContext(), MainTripActivity.class));
+                    break;
+                case R.id.search_navigation:
+                    startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                    break;
+                case R.id.settings_navigation:
+                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    break;
             }
+            return true;
         });
     }
 }
