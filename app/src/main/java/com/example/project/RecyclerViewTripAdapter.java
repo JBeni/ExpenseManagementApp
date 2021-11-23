8038a8f52f4ff065a94c2862c6167ddb8a945d33
JBeni <JBeni@users.noreply.github.com>
2021-11-23 13:34:35 +0200

    update recycler view trip adapter class

--- app/src/main/java/com/example/project/RecyclerViewTripAdapter.java
+++ app/src/main/java/com/example/project/RecyclerViewTripAdapter.java
@@ -3,10 +3,8 @@ package com.example.project;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -18,16 +16,16 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
-/**
- * The links was used to guide me and to adjust to the needs using parts of code from
- * https://stackoverflow.com/questions/26517855/using-the-recyclerview-with-a-database
- * https://developer.android.com/guide/topics/ui/layout/recyclerview
- *
- * https://github.com/android/views-widgets-samples/tree/main/RecyclerView
- * https://github.com/android/views-widgets-samples/tree/main/RecyclerViewAnimations
- * https://github.com/android/views-widgets-samples/tree/main/RecyclerViewSimple
- * https://www.dev2qa.com/android-cardview-with-image-and-text-example/
- **/
+/*
+  The links was used to guide me and to adjust to the needs using parts of code from
+    https://stackoverflow.com/questions/26517855/using-the-recyclerview-with-a-database
+    https://developer.android.com/guide/topics/ui/layout/recyclerview
+
+    https://github.com/android/views-widgets-samples/tree/main/RecyclerView
+    https://github.com/android/views-widgets-samples/tree/main/RecyclerViewAnimations
+    https://github.com/android/views-widgets-samples/tree/main/RecyclerViewSimple
+    https://www.dev2qa.com/android-cardview-with-image-and-text-example/
+*/
 public class RecyclerViewTripAdapter extends RecyclerView.Adapter<RecyclerViewTripAdapter.TripViewHolder> {
 
     private Context context;
@@ -50,57 +48,43 @@ public class RecyclerViewTripAdapter extends RecyclerView.Adapter<RecyclerViewTr
 
     @Override
     public void onBindViewHolder(@NonNull final TripViewHolder holder, @SuppressLint("RecyclerView") final int position) {
-        holder.trip_name_txt.setText(trips.get(position).getName().toString());
+        holder.trip_name_txt.setText(trips.get(position).getName());
 
-        holder.trip_main_grid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedTripViewModel sharedTripView = (SharedTripViewModel) context.getApplicationContext();
-                sharedTripView.setSharedTripId(String.valueOf(trips.get(position).getId()));
+        holder.trip_main_grid.setOnClickListener(view -> {
+            SharedTripViewModel sharedTripView = (SharedTripViewModel) context.getApplicationContext();
+            sharedTripView.setSharedTripId(String.valueOf(trips.get(position).getId()));
 
-                Intent intent = new Intent(context, MainTripExpensesActivity.class);
-                intent.putExtra("trip_id", String.valueOf(trips.get(position).getId()));
-                activity.startActivityForResult(intent, 1);
-            }
+            Intent intent = new Intent(context, MainTripExpensesActivity.class);
+            intent.putExtra("trip_id", String.valueOf(trips.get(position).getId()));
+            activity.startActivityForResult(intent, 1);
         });
 
-        /**
-         * https://stackoverflow.com/questions/34641240/toolbar-inside-cardview-to-create-a-popup-menu-overflow-icon
-         */
-        holder.trip_menu_options.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showPopupMenu(holder.trip_menu_options, position);
-            }
-        });
+        // https://stackoverflow.com/questions/34641240/toolbar-inside-cardview-to-create-a-popup-menu-overflow-icon
+        holder.trip_menu_options.setOnClickListener(view -> showPopupMenu(holder.trip_menu_options, position));
     }
 
-    /**
-     * https://stackoverflow.com/questions/34641240/toolbar-inside-cardview-to-create-a-popup-menu-overflow-icon
-     */
+    // https://stackoverflow.com/questions/34641240/toolbar-inside-cardview-to-create-a-popup-menu-overflow-icon
+    @SuppressLint("NonConstantResourceId")
     public void showPopupMenu(View view, int position) {
         PopupMenu popup = new PopupMenu(view.getContext(), view);
         popup.getMenuInflater().inflate(R.menu.trip_menu, popup.getMenu());
-        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.view_trip_card_button:
-                        Intent view_intent = new Intent(context, ViewTripActivity.class);
-                        setTripExtraIntentData(view_intent, position);
-                        activity.startActivityForResult(view_intent, 1);
-                        return true;
-                    case R.id.edit_trip_card_button:
-                        Intent edit_intent = new Intent(context, UpdateTripActivity.class);
-                        setTripExtraIntentData(edit_intent, position);
-                        activity.startActivityForResult(edit_intent, 1);
-                        return true;
-                    case R.id.delete_trip_card_button:
-                        confirmDeleteTripAndExpenses(String.valueOf(trips.get(position).getId()));
-                        return true;
-                    default:
-                        return false;
-                }
+        popup.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.view_trip_card_button:
+                    Intent view_intent = new Intent(context, ViewTripActivity.class);
+                    setTripExtraIntentData(view_intent, position);
+                    activity.startActivityForResult(view_intent, 1);
+                    return true;
+                case R.id.edit_trip_card_button:
+                    Intent edit_intent = new Intent(context, UpdateTripActivity.class);
+                    setTripExtraIntentData(edit_intent, position);
+                    activity.startActivityForResult(edit_intent, 1);
+                    return true;
+                case R.id.delete_trip_card_button:
+                    confirmDeleteTripAndExpenses(String.valueOf(trips.get(position).getId()));
+                    return true;
+                default:
+                    return false;
             }
         });
         popup.show();
@@ -124,23 +108,15 @@ public class RecyclerViewTripAdapter extends RecyclerView.Adapter<RecyclerViewTr
         builder.setMessage("Are you sure you want to delete the selected trip and the expenses related to it from the database?");
         builder.setCancelable(false);
 
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                SqliteDatabaseHandler databaseHandler = new SqliteDatabaseHandler(context);
-                databaseHandler.deleteTripAndExpenses(trip_id);
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            SqliteDatabaseHandler databaseHandler = new SqliteDatabaseHandler(context);
+            databaseHandler.deleteTripAndExpenses(trip_id);
 
-                Intent intent = new Intent(context, MainTripActivity.class);
-                activity.startActivityForResult(intent, 1);
-            }
+            Intent intent = new Intent(context, MainTripActivity.class);
+            activity.startActivityForResult(intent, 1);
         });
 
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
+        builder.setNegativeButton("No", (dialog, which) -> dialog.cancel());
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
@@ -152,7 +128,7 @@ public class RecyclerViewTripAdapter extends RecyclerView.Adapter<RecyclerViewTr
         return trips.size();
     }
 
-    class TripViewHolder extends RecyclerView.ViewHolder {
+    static class TripViewHolder extends RecyclerView.ViewHolder {
         TextView trip_name_txt;
         CardView trip_main_grid;
         ImageButton trip_menu_options;
