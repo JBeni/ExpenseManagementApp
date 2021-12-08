104fc8ffc78438d4a269b8a4d95e90a8a0409cff
JBeni <JBeni@users.noreply.github.com>
2021-12-08 12:58:14 +0200

    search activity changes

--- app/src/main/java/com/example/project/SearchActivity.java
+++ app/src/main/java/com/example/project/SearchActivity.java
@@ -1,11 +1,13 @@
 package com.example.project;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +18,7 @@ import android.widget.EditText;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SearchActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -43,28 +46,17 @@ public class SearchActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
 
         search_button.setOnClickListener(view -> {
-            trips.clear();
-
-            boolean b = trips.removeAll(trips);
-            int sizV = trips.size();
-
             search_name_enter = findViewById(R.id.search_text_view_field);
 
             if (search_name_enter.getText().toString().trim().length() == 0) {
-                getTripsData();
-                int sizVas = trips.size();
+                getTripsWithoutFilter();
             } else {
-                getSearchTripsData();
+                getSimpleSearchFilter(search_name_enter.getText().toString().trim());
             }
 
-            customSearchAdapter = new RecyclerViewSearchTripAdapter(SearchActivity.this,this, trips);
-            recyclerView.setAdapter(customSearchAdapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+            setRecyclerViewCustomAdapter();
         });
-        reset_button.setOnClickListener(view -> {
-            clearTripsData();
-            setRecycler();
-        });
+        reset_button.setOnClickListener(view -> clearTripsData());
 
         // https://medium.com/@makkenasrinivasarao1/bottom-navigation-in-android-application-with-activities-material-design-7a056b8cf38
         BottomNavigationView bottomNavigation = findViewById(R.id.navigation_bottom);
@@ -77,10 +69,34 @@ public class SearchActivity extends AppCompatActivity {
                 case R.id.search_navigation:
                     startActivity(new Intent(getApplicationContext(), SearchActivity.class));
                     break;
+                case R.id.settings_navigation:
+                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    break;
             }
             return true;
         });
     }
+
+    // https://stackoverflow.com/questions/61455381/how-to-replace-startactivityforresult-with-activity-result-apis
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+
+                if (
+                    Objects.requireNonNull(data).hasExtra("advance_search_name") ||
+                    Objects.requireNonNull(data).hasExtra("advance_search_destination") ||
+                    Objects.requireNonNull(data).hasExtra("advance_search_destination")
+                ) {
+                    String name = data.getStringExtra("advance_search_name");
+                    String destination = data.getStringExtra("advance_search_destination");
+                    String date = data.getStringExtra("advance_search_date");
+
+                    getAdvanceSearchFilter(name, destination, date);
+                }
+            }
+        });
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,54 +108,53 @@ public class SearchActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.search_advance_item:
-                advancedSearchDialog();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() != R.id.search_advance_item) {
+            return super.onOptionsItemSelected(item);
+        }
+        Intent intent = new Intent(SearchActivity.this, AdvanceSearchActivity.class);
+        someActivityResultLauncher.launch(intent);
+        return true;
+    }
+
+    private void getAdvanceSearchFilter(String name, String destination, String date) {
+        trips = databaseHandler.getAdvanceSearchFilter(name, destination, date);
+        if (trips.size() == 0) {
+            displayMessageFilter();
+        } else {
+            setRecyclerViewCustomAdapter();
         }
     }
 
-    private void advancedSearchDialog() {
+    private void getSimpleSearchFilter(String name) {
+        trips = databaseHandler.getSimpleSearchFilter(name);
+        if (trips.size() == 0) {
+            displayMessageFilter();
+        }
+    }
+
+    public void displayMessageFilter() {
         AlertDialog.Builder builder = new AlertDialog.Builder(SearchActivity.this);
-        builder.setTitle("Advance Search Filter");
         builder.setCancelable(false);
+        builder.setTitle("Search Filter");
+        builder.setMessage("There was no data with the value entered found in the database.");
 
-        Intent intent = new Intent(SearchActivity.this, AdvanceSearchActivity.class);
-        startActivity(intent);
-
-        builder.setMessage("Are you sure you want to delete all the data from database?");
-
-        builder.setPositiveButton("Search", (dialog, which) -> {
-            finish();
-        });
-        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-
+        builder.setNegativeButton("Close Dialog", (dialog, which) -> dialog.cancel());
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
 
-    private void setRecycler() {
+    private void getTripsWithoutFilter() {
+        trips = databaseHandler.getAllTrips();
+    }
+
+    private void setRecyclerViewCustomAdapter() {
         customSearchAdapter = new RecyclerViewSearchTripAdapter(SearchActivity.this,this, trips);
         recyclerView.setAdapter(customSearchAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
     }
 
-    private void getSearchTripsData() {
-        trips = new ArrayList<>();
-        trips = databaseHandler.getAllTrips();
-    }
-
-    private void getTripsData() {
-        trips = new ArrayList<>();
-        trips = databaseHandler.getAllTrips();
-    }
-
     private void clearTripsData() {
         trips = new ArrayList<>();
-        customSearchAdapter = new RecyclerViewSearchTripAdapter(SearchActivity.this,this, trips);
+        setRecyclerViewCustomAdapter();
     }
-
-
 }
